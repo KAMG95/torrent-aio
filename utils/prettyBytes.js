b8403049e9d7c9eb03b6c8017f6490f1ed413b02
KAMG95 <kareem013407@medicine.zu.edu.eg>
2020-11-10 21:43:26 +0200

    Update prettyBytes.js

--- utils/prettyBytes.js
+++ utils/prettyBytes.js
@@ -5,8 +5,8 @@ function prettyBytes(num) {
     units = ["B", "kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
   if (neg) num = -num;
   if (num < 1) return (neg ? "-" : "") + num + " B";
-  exponent = Math.min(Math.floor(Math.log(num) / Math.log(1000)), units.length - 1);
-  num = Number((num / Math.pow(1000, exponent)).toFixed(2));
+  exponent = Math.min(Math.floor(Math.log(num) / Math.log(100)), units.length - 1);
+  num = Number((num / Math.pow(100, exponent)).toFixed(2));
   unit = units[exponent];
   return (neg ? "-" : "") + num + " " + unit;
 }
